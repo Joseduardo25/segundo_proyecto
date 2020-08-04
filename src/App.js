52c8b23e9107bdb40821d13d1e3379a6c84caa6c
Eduardo Calachua <jocdwardo25@gmail.com>
2020-08-04 16:20:30 -0500

    material_UI

--- src/App.js
+++ src/App.js
@@ -1,22 +1,88 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
 
-function App() {
-   const[luz, setLuz] = useState(false)
-   function onOff(){
-     setLuz(!luz)
-   }
+
+const classes = useStyles();
+const useStyles = makeStyles({
+
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
   
-  return(
-    <div>
-     <div>
-        <img src={luz ? 'https://www.definicionabc.com/wp-content/uploads/2013/05/Foco.jpg' : 'https://promart.vteximg.com.br/arquivos/ids/253113-700-700/115271.jpg?v=636703713955570000'}/>
-        <button onClick={onOff}>{luz ? 'Apagar' : 'Encender'}</button>
-     </div>
-    </div>  
- )  
+  table: {
+    minWidth: 650,
+  },
+});
+
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
 }
 
-export default App;
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
+
+function SimpleTable() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Button variant="contained">Default</Button>
+      <Button variant="contained" color="primary">
+        Primary
+      </Button>
+      <Button variant="contained" color="secondary">
+        Secondary
+      </Button>
+      <Button variant="contained" disabled>
+        Disabled
+      </Button>
+      <Button variant="contained" color="primary" href="#contained-buttons">
+        Link
+      </Button>
+      <TableContainer component={Paper}>
+      <Table className={classes.table} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Dessert (100g serving)</TableCell>
+            <TableCell align="right">Calories</TableCell>
+            <TableCell align="right">Fat&nbsp;(g)</TableCell>
+            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow key={row.name}>
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.calories}</TableCell>
+              <TableCell align="right">{row.fat}</TableCell>
+              <TableCell align="right">{row.carbs}</TableCell>
+              <TableCell align="right">{row.protein}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+    </div>
+    
+  );
+}
+
+export default SimpleTable;
