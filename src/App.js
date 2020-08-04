250633038e6db0d0b6004bfe0690ff00e590258a
Eduardo Calachua <jocdwardo25@gmail.com>
2020-08-04 12:08:31 -0500

    toyota

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const carros = [
+      {
+        marca: 'toyota',
+        modelo: 'yaris',
+        logo: 'https://1000marcas.net/wp-content/uploads/2019/12/Toyota-logo.png'
+      }
+    ]
+     const[valueInput, setValueInput] = useState('')
+     const[vehiculos, setVehiculos] = useState(carros)
+     function add(e){
+       setValueInput(e.target.value)
+     }
+     function entrada(){
+       const array_vehiculos = [...vehiculos]
+       array_vehiculos.push({
+        marca: valueInput,
+        modelo: 'yaris',
+        logo: 'https://1000marcas.net/wp-content/uploads/2019/12/Toyota-logo.png'
+      })
+       setVehiculos(array_vehiculos)
+       setValueInput('')
+
+     }
+  
+  return(
+    <div>
+      <div>
+        {vehiculos.map((e)=>(
+        <div>
+          {e.marca}
+        </div>))}
+      </div>
+      <div>
+        <input onChange={add} value={valueInput}/>
+        <button onClick={entrada}>Agregar</button>
+      </div>
+    </div>  
+ )  
 }
 
 export default App;
