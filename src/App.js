3985778d8e0bb71f10d3b806037bb35b27981940
Eduardo Calachua <jocdwardo25@gmail.com>
2020-08-05 18:51:30 -0500

    material_UI_Form

--- src/App.js
+++ src/App.js
@@ -1,88 +1,64 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
-import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Avatar from '@material-ui/core/Avatar';
 
 
-const classes = useStyles();
-const useStyles = makeStyles({
 
-  root: {
-    '& > *': {
-      margin: theme.spacing(1),
+function App() {
+ 
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      '& > *': {
+        margin: theme.spacing(1),
+        width: '25ch',
+      },
     },
-  },
-  
-  table: {
-    minWidth: 650,
-  },
-});
-
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
-
-function SimpleTable() {
+    root_2: {
+      display: 'flex',
+      '& > *': {
+        margin: theme.spacing(1),
+      },
+    },
+  }));
   const classes = useStyles();
+  const usuario = ['https://i.ytimg.com/vi/uhuxVFktLk4/maxresdefault.jpg']
+  const[valueInput, setValueInput] = useState('')
+  const[array, setArray] = useState(usuario)
+  
+  const onClickbuttonForm = () => {
+    const copyArray = [...array]
+    copyArray.push(valueInput)
+    setArray(copyArray)
+    setValueInput('')
+  }
+  const onSubmitForm = (e) => {
+    e.preventDefault()
+    onClickbuttonForm()
+  }
+  const onChangeInputForm = (e) => {
+    setValueInput(e.target.value)
+  }
 
   return (
-    <div className={classes.root}>
-      <Button variant="contained">Default</Button>
-      <Button variant="contained" color="primary">
-        Primary
-      </Button>
-      <Button variant="contained" color="secondary">
-        Secondary
-      </Button>
-      <Button variant="contained" disabled>
-        Disabled
-      </Button>
-      <Button variant="contained" color="primary" href="#contained-buttons">
-        Link
-      </Button>
-      <TableContainer component={Paper}>
-      <Table className={classes.table} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.name}>
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-    </div>
+    <form className={classes.root} onSubmit={onSubmitForm} noValidate autoComplete="off">
+      <div className={classes.root_2}>
+      {array.map((e)=>(<Avatar alt={e} src={e}/>))}
+      </div>
+      <TextField 
+        id="standard-basic" 
+        label="Standard"
+        value={valueInput}
+        onChange={onChangeInputForm}
+         />
+         <button>
+           Agregar
+         </button>
+      
+    </form>
     
   );
 }
 
-export default SimpleTable;
+
+export default App;
